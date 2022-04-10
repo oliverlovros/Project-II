57103396212882ff7e6064fbe1ef202071b47791
dani11torres <66941005+dani11torres@users.noreply.github.com>
2022-04-10 13:34:17 +0200

    Update Forces_module.f90

--- Serie/Code/Modules/Forces_module.f90
+++ Serie/Code/Modules/Forces_module.f90
@@ -2,76 +2,70 @@ module forces_module
 
 use Constants_module
 use pbc_module
+!--------------------------!
+use paralel
+!--------------------------!
+
 implicit none
-
 contains
+SUBROUTINE L_J(dr,force,Upot,cutoff)
+    IMPLICIT NONE
+    REAL*8 dr,cutoff,force,Upot
+    force=0d0 !Establecemos la fuerza inicial a 0           
+    Upot=0d0  !Establecemos la energia potencial a 0
+    IF (dr<cutoff) THEN !Si es menor que el cut-off calculamos la interaccion
+      force=(48d0/dr**14d0)-(24d0/dr**8d0)
+      Upot=4d0*((1d0/dr**12d0)-(1d0/dr**6d0))
+    END IF
+    RETURN
+END SUBROUTINE L_J
 
-subroutine LJ_potential(npart,positions,cutoff,length,pbc_on, Upot, force)
 
-    !------------------------------------------------------------------------------------------------------------------------------!
-    ! Author: Daniel Conde
-    ! About:
-    ! The LJ_potential subroutine calculates the potential energy and the forces between particles, which are subjected to a Lennard-
-    ! Jones potential.
-    ! Input variables:
-    !   npart: total number of particles in the system
-    !   positions(npart,3): matrix containing the positions of all the particles (npart)
-    !   cutoff: interaction radius
-    !   length: length of each side of the box
-    !   pbc_on: if 1, periodic boundary conditions are applied
-    ! Output variables:
-    !   Upot: potential energy of the system
-    !   force(npart,3): matrix to obtain the force applied on each particle
-    !------------------------------------------------------------------------------------------------------------------------------!
 
-    implicit none
-    integer, intent(in) :: npart, pbc_on
-    double precision, intent(in) :: positions(npart,3), cutoff, length
-    double precision :: Upot, force(npart,3)
-    integer :: i, j, k
-    double precision :: dr(3), dr2, dr6, dr8, dr12, dr14
-   
-    ! Energy is initialized
-    Upot = 0.0
-    ! Force is initialized
-    force = 0.d0
-    ! Loop that loops through all interactions
-     !$omp parallel private(dr,dr6,dr8, dr12, dr14,dr2) 
-!$omp do schedule(dynamic,4)  reduction(+:Upot) reduction(+:force)
-!algo mal en la parelizacion? No va más rápido pero parece que da los mismos resultados, probar con sistema más grande y más tiempo
-!La actual version del programa no funciona, el error parece estar en el main (pues una anterior iba)
-    do i = 1, npart-1
-        do j = i+1, npart
-        ! Difference between two particles calculation
-            do k = 1, 3
-                dr(k) = positions(i,k) - positions(j,k)
-            end do
-            ! PBC are applied
-            if(pbc_on == 1) call pbc(dr,length)
-            ! Distance calculation
-            dr2 = dr(1)**2 + dr(2)**2 + dr(3)**2
-            ! Cutoff correction
-            if(dr2 <= cutoff**2) then
-                dr6 = dr2**3
-                dr8 = dr2**4
-                dr12 = dr2**6
-                dr14 = dr2**7
-                ! Potential energy
-                Upot = Upot+4.d0*(1.d0/dr12 - 1.d0/dr6) - 4.d0*( 1.d0/cutoff**12 - 1.d0/cutoff**6)
-                ! Force particle i
-                force(i,1) = force(i,1) + (48.d0/dr14 - 24.d0/dr8)*dr(1)
-                force(i,2) = force(i,2) + (48.d0/dr14 - 24.d0/dr8)*dr(2)
-                force(i,3) = force(i,3) + (48.d0/dr14 - 24.d0/dr8)*dr(3)
-                ! Force particle j
-                force(j,1) = force(j,1) - (48.d0/dr14 - 24.d0/dr8)*dr(1)
-                force(j,2) = force(j,2) - (48.d0/dr14 - 24.d0/dr8)*dr(2)
-                force(j,3) = force(j,3) - (48.d0/dr14 - 24.d0/dr8)*dr(3)
-            end if
-        end do
-    end do
-    !$omp end do
-    !$omp end parallel
-    return 
+
+SUBROUTINE LJ_potential(n_particles,positions,cutoff,length,pbc_on, Upot, F) 
+! Compute the forces, the potential energy of the
+! system and the pressure taking into account PBC
+    IMPLICIT NONE
+    integer, intent(in):: n_particles,pbc_on
+    double precision, intent(in) :: positions(n_particles,3), length
+    INTEGER :: i,j,par
+    REAL*8 :: cutoff,pot,Upot,pressure
+    REAL*8 :: dx,dy,dz,d,ff
+    double precision :: dr(3)
+    REAL*8, DIMENSION(:,:) :: F
+    F=0d0
+    Upot=0d0
+    pressure=0.0
+    !Symmetric Matrix Energy
+    DO par=index_matrix2(workerid+1,1), index_matrix2(workerid+1,2)
+      i=pairindex(par,1) !Particle i on the interaction
+      j=pairindex(par,2) !Particle j on the interaction
+      dr(1) = (positions(i,1) - positions(j,1))
+      dr(2) = (positions(i,2) - positions(j,2))
+      dr(3) =  (positions(i,3) - positions(j,3))
+      print*,'dr'
+      if(pbc_on == 1) call pbc(dr,length)
+      ! Distance calculation
+      d = (dr(1)**2 + dr(2)**2 + dr(3)**2)**0.5
+      CALL L_J(d,ff,pot,cutoff)
+            F(i,1)=F(i,1)+ff*dr(1)
+            F(i,2)=F(i,2)+ff*dr(2)
+            F(i,3)=F(i,3)+ff*dr(3)
+            F(j,1)=F(j,1)-ff*dr(1)
+            F(j,2)=F(j,2)-ff*dr(2)
+            F(j,3)=F(j,3)-ff*dr(3)
+            Upot=Upot+pot
+            pressure=pressure+(ff*dr(1)**2d0+ff*dr(2)**2d0+ff*dr(3)**2d0)
+    END DO
+    !Compute the total interaction force for each particle
+    call MPI_ALLREDUCE( F, F,n_particles*3,MPI_DOUBLE_PRECISION,MPI_SUM,MPI_COMM_WORLD,ierror)
+    ! Compute the sum for all the workers
+    call MPI_REDUCE(Upot, Upot,1,MPI_DOUBLE_PRECISION,MPI_SUM,0,MPI_COMM_WORLD,ierror)
+    call MPI_REDUCE(pressure,pressure,1,MPI_DOUBLE_PRECISION,MPI_SUM,0,MPI_COMM_WORLD,ierror)
+
+    return
+
 end subroutine LJ_potential
 
 subroutine Kinetic_Energy(nparts,velocity,kin_E)
@@ -108,55 +102,5 @@ subroutine Kinetic_Energy(nparts,velocity,kin_E)
     return
    
 end subroutine Kinetic_Energy
-
-
-
-
-
-subroutine pressure(n,rho,temp,l,cutoff,pos,pres)
-
-    !------------------------------------------------------------------------------------------------------------------------------!
-    ! Author: Daniel Conde
-    ! About:
-    ! The pressure subroutine calculates the pressure of a npart system.
-    ! Input variables:
-    !   n: number of total particles
-    !   rho: density of the system
-    !   temp: temperature of the system
-    !   l: length of the box
-    !   cutoff: interaction radius
-    !   pos: position of each particle
-    ! Output variables:
-    !   pres: system pressure
-    !------------------------------------------------------------------------------------------------------------------------------!
-
-    implicit none
-    ! Variables
-    integer :: n
-    double precision :: rho, temp, l, cutoff, pos(n,3)
-    double precision :: pres
-    integer :: i, j
-    double precision :: dr(3), dr2, force(3)
-
-    pres = 0.d0
-    do i=1,n-1
-        do j=i+1,n
-            dr = pos(i,:)-pos(j,:)
-            call pbc(dr,l)
-            dr2 = sum(dr**2)
-            if (dr2 < cutoff**2) then
-                force = (48.d0/dr2**7 - 24.d0/dr2**4)*dr
-                pres = pres + sum(dr*force)
-            end if
-        end do
-    end do
-    pres=pres+16/3.*pi*rho**2*(2/3.*(1./cutoff)**9)-(1/cutoff)**3/(1/(3*l**3))! Added for cut-off corrections
-    pres = rho*temp + pres/(3.d0*l**3)
-
-    return
-
-end subroutine pressure
-
-
 end module forces_module
 
