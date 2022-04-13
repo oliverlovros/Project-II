765dde80051dfbc9cea775ef1733684a49103d3b
altepi97 <alex.teruel.pinol@gmail.com>
2022-04-13 19:38:57 +0200

    modulos de integracion y g(r) paralelizados

--- Paralel/Code/Modules/Gofr_module.f90
+++ Paralel/Code/Modules/Gofr_module.f90
@@ -1,97 +1,105 @@
 module pair_distribution_function_module
 
+    use Parallel_module
     use pbc_module
     implicit none
 
     contains
 
-! pair distribution function
-subroutine pair_distribution_function(class,nparts,measures,rho,positions,nboxes,gofr,length,cutoff,dr,results)
+    ! pair distribution function
+    subroutine pair_distribution_function(class,nparts,measures,rho,positions,nboxes,gofr,length,cutoff,dr,results)
 
-    ! information:
-    !   case 1: initialize gofr
-    !   case 2: sample distances
-    !   case 3: normalize gofr
-    !   case 4: write results
+        ! information:
+        !   case 1: initialize gofr
+        !   case 2: sample distances
+        !   case 3: normalize gofr
+        !   case 4: write results
 
-    implicit none
-    ! in/out variables
-    character(len=50), intent(in)   :: results
-    integer, intent(in)             :: class, nparts, nboxes
-    double precision, intent(in)    :: rho, positions(nparts,3), length, cutoff
-    integer, intent(inout)          :: measures
-    double precision, intent(inout) :: gofr(nboxes), dr
-    !---------------------------------------------------------------------------!
-    ! internal variables
-    integer :: i, j, xi
-    double precision :: x1(3), x2(3), xr, vol
-    double precision, parameter :: pi = 4.d0*datan(1.d0)
+        implicit none
+        ! in/out variables
+        character(len=50), intent(in)   :: results
+        integer, intent(in)             :: class, nparts, nboxes
+        double precision, intent(in)    :: rho, positions(nparts,3), length, cutoff
+        integer, intent(inout)          :: measures
+        double precision, intent(inout) :: gofr(nboxes), dr
+        !---------------------------------------------------------------------------!
+        ! internal variables
+        integer :: i, j, k, xi
+        double precision :: x1(3), x2(3), xr, vol
+        double precision :: total_gofr(nboxes)
+        double precision, parameter :: pi = 4.d0*datan(1.d0)
 
-    select case (class)
+        select case (class)
 
-        case (1)
+            case (1)
 
-        measures = 0
-        gofr = 0.d0
-        dr = cutoff/dble(nboxes)
+            measures = 0
+            gofr = 0.d0
+            dr = cutoff/dble(nboxes)
 
-        case (2)
+            case (2)
 
-        measures = measures + 1
-        do i = 1, nparts-1
-            do j = i+1, nparts
+            measures = measures + 1
+            do k = interactions(rank,1), interactions(rank,2)
+                i = particles_interaction(k,1)
+                j = particles_interaction(k,2)
                 ! calculate the distance between particles i and j with pbc
-                x1 = positions(i,:)
-                x2 = positions(j,:)
+                x1(1) = positions(i,1); x1(2) = positions(i,2); x1(3) = positions(i,3)
+                x2(1) = positions(j,1); x2(2) = positions(j,2); x2(3) = positions(j,3)
                 xr = distance(x1,x2,length)
-                ! if the distance is less than L/2 add 1 in the corresponding box
+                ! if the distance is less than cutoff radius add 1 in the corresponding box
                 if (xr < cutoff) then
                     xi = int(xr/dr) + 1
                     gofr(xi) = gofr(xi) + 2
                 endif
-            enddo
-        enddo
+            end do
 
-        case (3)
+            case (3)
 
-        do i = 1, nboxes
-            vol = 4.d0/3.d0*pi*dr**3*dble(i**3-(i-1)**3)
-            gofr(i) = gofr(i)/(dble(measures*nparts)*vol*rho)
-        end do
+            ! sum all processors contributions
+            call MPI_allreduce(gofr,total_gofr,size(gofr),MPI_DOUBLE_PRECISION,MPI_SUM,MPI_COMM_WORLD,ierror)
+            gofr = total_gofr
 
-        case (4)
+            do i = 1, nboxes
+                vol = 4.d0/3.d0*pi*dr**3*dble(i**3-(i-1)**3)
+                gofr(i) = gofr(i)/(dble(measures*nparts)*vol*rho)
+            end do
 
-        open(99,file=results)
-        do i = 1, nboxes
-            xr = dr*i - 0.5d0*dr
-            write(99,*) xr, gofr(i)
-        end do
-        close(99)
+            case (4)
 
-        case default
+            if (rank == 0) then
+                open(99,file=results)
+                do i = 1, nboxes
+                    xr = dr*i - 0.5d0*dr
+                    write(99,*) xr, gofr(i)
+                end do
+                close(99)
+            end if
 
-        print*, "Pair distribution function: Wrong case. Case must be 1, 2, 3, or 4."
+            case default
 
-    end select
+            print*, "Pair distribution function: Wrong case. Case must be 1, 2, 3, or 4."
 
-    return
+        end select
 
-end subroutine pair_distribution_function
+        return
 
-! distance between 2 particles with pbc
-double precision function distance(x1,x2,l)
+    end subroutine pair_distribution_function
 
-implicit none
-double precision, intent(in) :: x1(3), x2(3), l
-double precision, external   :: pbc_func
-double precision             :: x12(3)
+    ! distance between 2 particles with pbc
+    double precision function distance(x1,x2,l)
 
-x12 = x1-x2
-call pbc(x12,l)
-distance = dsqrt(sum(x12**2))
+        implicit none
+        double precision, intent(in) :: x1(3), x2(3), l
+        double precision, external   :: pbc_func
+        double precision             :: x12(3)
 
-return
+        x12 = x1-x2
+        call pbc(x12,l)
+        distance = dsqrt(sum(x12**2))
 
-end function distance
+        return
+
+    end function distance
 
 end module pair_distribution_function_module
--- Paralel/Code/Modules/Integrator_module.f90
+++ Paralel/Code/Modules/Integrator_module.f90
@@ -1,6 +1,7 @@
 module Integrator_module
     
     ! parameters
+    use Parallel_module
     use pbc_module
     use forces_module
     implicit none
@@ -47,92 +48,110 @@ module Integrator_module
 
     subroutine velocity_verlet_andersen(nparts,box_L,cutoff,nu,sigma,dt,positions,velocities,pot_E,forces,therm_on)
 
-     !-----------------------------------------------------------------------------------------------------------------------------!
-     ! Author: Àlex Teruel
-     ! About:
-     ! The velocity_verlet_andersen subroutine applies the velocity Verlet algorithm to integrate an instant of time through an
-     ! interacting particle under a Lennard-Jones potential and an Andersen thermostat.
-     ! Input variables:
-     !   nparts: total number of particles in the system
-     !   box_L: length of each side of the box
-     !   cutoff: interaction radius
-     !   nu: probability of accepting a change of velocity
-     !   sigma: distribution deviation
-     !   dt: time difference
-     !   therm_on: if it is 1, we apply the thermostat, if not, no.
-     ! Modified input and output variables:
-     !   positions(npart,3): matrix containing the positions of the npart system
-     !   velocities(nparts,3): matrix containing the velocities in each direction for each particle 
-     !   pot_E: potential energy of the system
-     !   forces(nparts,3): matrix to obtain the force applied on each particle of the system
-     !-----------------------------------------------------------------------------------------------------------------------------!
-    
-        implicit none
-        ! Input and output variables
-        integer :: nparts, therm_on
-        double precision :: box_L, cutoff, nu, sigma, dt
-        double precision :: positions(nparts,3), velocities(nparts,3)
-        double precision :: pot_E, forces(nparts,3)
-        ! Internal variables (subroutine)
-        double precision :: newforces(nparts,3)
-    
-        ! New positions are calculated
-        positions = positions + velocities*dt + 0.5d0*forces*dt**2
-        ! Periodic Boundary Conditions are applied
-        call pbc_nparts(nparts,positions,box_L)
-        ! New forces and potential energy are calculated
-        call LJ_potential(nparts,positions,cutoff,box_L,1,pot_E,newforces)
-        ! New velocities are calculated
-        velocities = velocities + 0.5d0*dt*(forces + newforces)
-        ! Force values are reassigned
-        forces = newforces
-        ! Thermostat is applied (sometimes)
-        if(therm_on == 1) call therm_Andersen(nparts,velocities,nu,sigma)
-    
-        return
-    
-    end subroutine velocity_verlet_andersen
-
-    subroutine euler_andersen(nparts,box_L,cutoff,nu,sigma,dt,positions,velocities,pot_E,forces,therm_on)
-
-     !-----------------------------------------------------------------------------------------------------------------------------!
-     ! Author: Àlex Teruel
-     ! About:
-     ! The euler_andersen subroutine applies the Euler algorithm to integrate an instant of time through an
-     ! interacting particle under a Lennard-Jones potential and an Andersen thermostat.
-     ! Input variables:
-     !   nparts: total number of particles in the system
-     !   box_L: length of each side of the box
-     !   cutoff: interaction radius
-     !   nu: probability of accepting a change of velocity
-     !   sigma: distribution deviation
-     !   dt: time difference
-     !   therm_on: if it is 1, we apply the thermostat, if not, no.
-     ! Modified input and output variables:
-     !   positions(npart,3): matrix containing the positions of the npart system
-     !   velocities(nparts,3): matrix containing the velocities in each direction for each particle 
-     !   pot_E: potential energy of the system
-     !   forces(nparts,3): matrix to obtain the force applied on each particle of the system
-     !-----------------------------------------------------------------------------------------------------------------------------!
-    
-        implicit none
-        ! Input and output variables
-        integer :: nparts, therm_on
-        double precision :: positions(nparts,3), velocities(nparts,3), forces(nparts,3), dt, box_L, cutoff, pot_E, nu, sigma
-    
-        ! new positions
-        positions = positions + velocities*dt + 0.5d0*forces*dt**2
-        ! new velocities
-        velocities = velocities + forces*dt
-        ! periodic boundary conditions
-        call pbc_nparts(nparts,positions,box_L)
-        ! new forces and potential energy
-        call LJ_potential(nparts,positions,cutoff,box_L,1,pot_E,forces)
-        ! thermostat (sometimes)
-        if (therm_on == 1) call therm_Andersen(nparts,velocities,nu,sigma)
-
-        return
-    
-    end subroutine euler_andersen
+        !-----------------------------------------------------------------------------------------------------------------------------!
+        ! Author: Àlex Teruel
+        ! About:
+        ! The velocity_verlet_andersen subroutine applies the velocity Verlet algorithm to integrate an instant of time through an
+        ! interacting particle under a Lennard-Jones potential and an Andersen thermostat.
+        ! Input variables:
+        !   nparts: total number of particles in the system
+        !   box_L: length of each side of the box
+        !   cutoff: interaction radius
+        !   nu: probability of accepting a change of velocity
+        !   sigma: distribution deviation
+        !   dt: time difference
+        !   therm_on: if it is 1, we apply the thermostat, if not, no.
+        ! Modified input and output variables:
+        !   positions(npart,3): matrix containing the positions of the npart system
+        !   velocities(nparts,3): matrix containing the velocities in each direction for each particle 
+        !   pot_E: potential energy of the system
+        !   forces(nparts,3): matrix to obtain the force applied on each particle of the system
+        !-----------------------------------------------------------------------------------------------------------------------------!
+       
+           implicit none
+           ! Input and output variables
+           integer :: nparts, therm_on
+           double precision :: box_L, cutoff, nu, sigma, dt
+           double precision :: positions(nparts,3), velocities(nparts,3)
+           double precision :: pot_E, forces(nparts,3)
+           ! Internal variables (subroutine)
+           double precision :: newforces(nparts,3)
+           integer :: i
+       
+           ! New positions are calculated
+           do i = particles(rank,1), particles(rank,2)
+               positions(i,:) = positions(i,:) + velocities(i,:)*dt + 0.5d0*forces(i,:)*dt**2
+               ! Periodic Boundary Conditions are applied
+               call pbc(positions(i,:),box_L)
+           end do
+           ! New positions must be shared to all processors to compute new forces
+           do i = 1, 3
+               call MPI_ALLGATHERV(positions(particles(rank,1):particles(rank,2),i), particles(rank,2)-particles(rank,1)+1, &
+               MPI_DOUBLE_PRECISION, positions(:,i), num_send, displac, MPI_DOUBLE_PRECISION, MPI_COMM_WORLD, ierror)
+           end do
+           ! New forces and potential energy are calculated
+           call LJ_potential(nparts,positions,cutoff,box_L,1,pot_E,newforces)
+           ! New velocities are calculated
+           do i = particles(rank,1), particles(rank,2)
+               velocities(i,:) = velocities(i,:) + 0.5d0*dt*(forces(i,:) + newforces(i,:))
+           end do
+           ! Force values are reassigned
+           forces = newforces
+           ! Thermostat is applied (sometimes)
+           if(therm_on == 1) call therm_Andersen(nparts,velocities,nu,sigma)
+       
+           return
+       
+       end subroutine velocity_verlet_andersen
+   
+       subroutine euler_andersen(nparts,box_L,cutoff,nu,sigma,dt,positions,velocities,pot_E,forces,therm_on)
+   
+        !-----------------------------------------------------------------------------------------------------------------------------!
+        ! Author: Àlex Teruel
+        ! About:
+        ! The euler_andersen subroutine applies the Euler algorithm to integrate an instant of time through an
+        ! interacting particle under a Lennard-Jones potential and an Andersen thermostat.
+        ! Input variables:
+        !   nparts: total number of particles in the system
+        !   box_L: length of each side of the box
+        !   cutoff: interaction radius
+        !   nu: probability of accepting a change of velocity
+        !   sigma: distribution deviation
+        !   dt: time difference
+        !   therm_on: if it is 1, we apply the thermostat, if not, no.
+        ! Modified input and output variables:
+        !   positions(npart,3): matrix containing the positions of the npart system
+        !   velocities(nparts,3): matrix containing the velocities in each direction for each particle 
+        !   pot_E: potential energy of the system
+        !   forces(nparts,3): matrix to obtain the force applied on each particle of the system
+        !-----------------------------------------------------------------------------------------------------------------------------!
+       
+           implicit none
+           ! Input and output variables
+           integer :: nparts, therm_on
+           double precision :: positions(nparts,3), velocities(nparts,3), forces(nparts,3), dt, box_L, cutoff, pot_E, nu, sigma
+           ! Internal variables
+           integer :: i
+       
+           ! new positions and velocities
+           do i = particles(rank,1), particles(rank,2)
+               positions(i,:) = positions(i,:) + velocities(i,:)*dt + 0.5d0*forces(i,:)*dt**2
+               velocities(i,:) = velocities(i,:) + forces(i,:)*dt
+               ! Periodic Boundary Conditions are applied
+               call pbc(positions(i,:),box_L)
+           end do
+           ! New positions must be shared to all processors to compute new forces
+           do i = 1, 3
+               call MPI_ALLGATHERV(positions(particles(rank,1):particles(rank,2),i), particles(rank,2)-particles(rank,1)+1, &
+               MPI_DOUBLE_PRECISION, positions(:,i), num_send, displac, MPI_DOUBLE_PRECISION, MPI_COMM_WORLD, ierror)
+           end do
+           ! new forces and potential energy
+           call LJ_potential(nparts,positions,cutoff,box_L,1,pot_E,forces)
+           ! Thermostat is applied (sometimes)
+           if(therm_on == 1) call therm_Andersen(nparts,velocities,nu,sigma)
+   
+           return
+       
+       end subroutine euler_andersen
 
 end module Integrator_module
